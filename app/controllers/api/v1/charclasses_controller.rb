b5697ed0714a8d3d9ac9a1207d4f906fb12f537e
kreestoff <christopher.l.mcintosh@outlook.com>
2019-05-28 15:41:37 -0500

    added race and class

---
+++ app/controllers/api/v1/charclasses_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::CharclassesController < ApplicationController
+    before_action :find_charclass, only: [:update]
+    def index
+      @charclasses = Charclass.all
+      render json: @charclasses
+    end
+
+    def update
+      @charclass.update(race_params)
+      if @charclass.save
+        render json: @charclass, status: :accepted
+      else
+        render json: { errors: @charclass.errors.full_messages }, status: :unprocessible_entity
+      end
+    end
+
+    private
+
+    def charclass_params
+      params.permit(:names)
+    end
+
+    def find_charclass
+      @charclass = Charclass.find(params[:id])
+    end
+end
