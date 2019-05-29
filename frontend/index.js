4a1950a31a669367f4a792c28adbacd470991147
github username <github email address>
2019-05-29 11:29:51 -0500

    started dropdown

--- frontend/index.js
+++ frontend/index.js
@@ -4,18 +4,34 @@ document.addEventListener('DOMContentLoaded', () => {
 const CharacterURL = "http://localhost:3000/api/v1/characters"
 const CharClassURL = "http://localhost:3000/api/v1/charclasses"
 const RacesURL = "http://localhost:3000/api/v1/races"
-const dropDown = document.querySelector("#dropdown")
+const dropDown = document.querySelector(".chardropdown")
+const selectTag = document.createElement('select')
+
+let option = document.createElement('option')
+option.innerText = "Select a Character"
+option.selected = true
+option.disabled = true
+selectTag.append(option)
 
 
-fetch(CharacterURL)
-.then((resp) => resp.json)
-.then((respJson) => {
-    debugger
-    respJson.forEach((char) => {
-        
+function fetchingCharacters(){
+    fetch(CharacterURL)
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(char => renderCharacter(char))
     })
+}
 
-})
+function renderCharacter(char){
+    let opt = document.createElement("option")
+    opt.innerText = char.name
+    selectTag.append(opt)
+}
 
+// selectTag.addEventListener('change',(e)=>{
+    
+// })
 
+dropDown.append(selectTag)
+fetchingCharacters()
 })
