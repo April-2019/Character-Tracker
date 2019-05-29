9d21fcd141587034b66d2a41cd8705f9a88f294f
github username <github email address>
2019-05-29 10:54:34 -0500

    db fix

--- app/models/charclass.rb
+++ app/models/charclass.rb
@@ -1,3 +1,4 @@
 class Charclass < ApplicationRecord
     has_many :characters
+    has_many :races, through: :characters
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :characters, :charclasses, :races,  only: [:index, :update]
+      resources :characters, :charclasses, :races
     end
   end
 end
