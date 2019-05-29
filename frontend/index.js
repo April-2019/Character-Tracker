1d8e30b5968856054a4b9c3896826eaf1c6f1023
github username <github email address>
2019-05-29 10:21:41 -0500

    changes to css grid

---
+++ frontend/index.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+//assigning form to variable
+let form = document.querySelector('#character_form')
+// strength value
+form[0].value
+//dexterity
+form[1].value
+//constitution
+form[2].value
+//intelligence
+form[3].value
+//wisdom
+form[4].value
+//charisma
+form[5].value
+//skills
+form[6].value
+//name
+form[7].value
+//notes
+form[8].value
+//hitpoints
+form[9].value
+//inventory
+form[10].value
+
+
+
+
+
+})
